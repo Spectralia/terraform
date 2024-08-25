798e6e264fee0c3233d5b9c57e3fcd5cdcff5477
Spectralia <oracle.code@pm.me>
2024-08-25 16:00:24 -0700

    listavialable
    add listmodels fun

--- lambdas/my_lambda/index.py
+++ lambdas/my_lambda/index.py
@@ -1,29 +1,19 @@
 import json
 import boto3
-import time
+
+# import time
 
 
 def main(event, context):
-    start_time = time.time()  # 获取开始时间
+    response_body = listAvailableModels(event, context)
+    return {"statusCode": 200, "body": json.dumps(response_body)}
 
-    result = "Hello from Lambda!"
 
-    http_method = event["httpMethod"]
 
-    client_body = event["body"]
-    if client_body and http_method:
-        result = "{}({})".format(client_body, http_method)
-    else:
-        result = "No body({})".format(http_method)
-
-    end_time = time.time()  # 获取结束时间
-    execution_time = "(程序运行时间：%.2f 秒)" % (
-        end_time - start_time
-    )  # 计算程序运行时间
-    print(execution_time)
-
-    return {
-        "statusCode": 200,
-        "body": "{}\n\n{}".format(execution_time, result),
-        "headers": {"Content-Type": "text/plain"},
-    }
+def listAvailableModels(event, context):
+    client_bedrock = boto3.client("bedrock")
+    response = client_bedrock.list_foundation_models()
+    response_body = []
+    for summary in response["modelSummaries"]:
+        response_body.append({summary["providerName"]: summary["modelId"]})
+    return response_body
