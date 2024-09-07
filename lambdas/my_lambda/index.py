e182c60d54d05528890f1629e214502185b2c4bd
Spectralia <oracle.code@pm.me>
2024-09-06 19:20:06 -0700

    Titan text API

--- lambdas/my_lambda/index.py
+++ lambdas/my_lambda/index.py
@@ -1,10 +1,11 @@
 import json
+import time
 import boto3
 import base64
 import datetime
 import random
 
-import boto3.s3
+import boto3
 
 # import time
 
@@ -16,64 +17,48 @@ fileNum = random.randint(0, 4294967295)
 
 
 def main(event, context):
+    start_time = time.time()
     response_body = "No body"
-    client_body = event["body"]
     # http_method = event.get("httpMethod", None)
     http_method = event["httpMethod"]
+    client_body = event["body"]
     # http_method = "POST"
     if client_body and http_method:
         if http_method == "POST":
             response_body = generateImageBySDML(client_body)
+    end_time = time.time()
+    execution_time = "Program Run Time : %.2f sec" % (end_time - start_time)
+
     return {
         "statusCode": 200,
-        "body": response_body,
+        "body": "{}\n\n{}".format(execution_time, response_body),
         "headers": {"Content-Type": "text/plain"},
     }
 
 
-def generateImageBySDML(client_body):
+def generateImageBySDML(client_prompt):
     client_bedrock = boto3.client("bedrock-runtime")
     client_s3 = boto3.client("s3")
-    input_prompt = client_body
-    seed = random.randint(0, 4294967295)
-
+    input_prompt = """{}""".format(client_prompt)
     response_bedrock = client_bedrock.invoke_model(
         contentType="application/json",
         accept="application/json",
-        modelId="stability.stable-diffusion-xl-v1",
+        # modelId="stability.stable-diffusion-xl-v1",
+        modelId="amazon.titan-text-premier-v1:0",
         body=json.dumps(
             {
-                "text_prompts": [{"text": input_prompt}],
-                # "cfg_scale": 20,
-                # "samples": 1,
-                # "steps": 30,
-                # "seed":seed
-                "height": 768,
-                "width": 1344,
-                "style_preset": "photographic",
+                "inputText": input_prompt,
+                "textGenerationConfig": {
+                    "maxTokenCount": 1000,
+                    "stopSequences": [],
+                    "temperature": 0.7,
+                    "topP": 0.9,
+                },
             }
         ),
     )
-    response_bedrock_byte = json.loads(response_bedrock["body"].read())
-    response_bedrock_base64 = response_bedrock_byte["artifacts"][0]["base64"]
-    response_bedrock_finalImage = base64.b64decode(response_bedrock_base64)
-
-    # imageName = "imageName_" + now.strftime("%Y%m%d%H%M%S") + ".png"
-    imageName = "imageName_" + str(fileNum) + ".png"
-    response_s3 = client_s3.put_object(
-        Bucket=s3Bucket, Body=response_bedrock_finalImage, Key=imageName
-    )
-
-    generate_presigned_url = client_s3.generate_presigned_url(
-        "get_object",
-        Params={
-            "Bucket": s3Bucket,
-            "Key": imageName,
-            "ResponseCacheControl": "no-cache",
-            "ResponseContentDisposition": "inline",
-            "ResponseContentType": "image/png",
-        },
-        ExpiresIn=3600,
-    )
-    print(generate_presigned_url)
-    return generate_presigned_url
+    response_body = json.loads(response_bedrock["body"].read())
+    finish_reason = response_body.get("error")
+    if finish_reason is not None:
+        raise Exception(f"Text generation error. Error is {finish_reason}")
+    return response_body["results"][0]["outputText"]
